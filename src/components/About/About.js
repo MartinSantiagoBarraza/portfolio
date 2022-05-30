e9f9c8a939d03a8ad94fc6f038853a9c3e6da857
kloppista <martinsbarraza@gmail.com>
2022-05-29 22:35:19 -0300

    update info and node-sass

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import './About.scss';
-import { RoughNotation } from "react-rough-notation";
+import React from "react";
+import "./About.scss";
 
 const About = () => {
-    return (
-        <div className="about" id="about">
-                <h2>About Me</h2>
-            <p>Hello! I'm 24 years old,  working as an IT Recruiter at <strong>Avalith</strong> and studying Web Development at <strong>UNLaM</strong>. My goal is to work on a company as a <RoughNotation type="highlight" animate="true" color="lightgreen" show="true" customElement="span"><strong>Full Stack Developer</strong></RoughNotation>.</p>
-            <p>I'm <RoughNotation type="highlight" animate="true" color="coral" show="true" customElement="span">passionate</RoughNotation> about <strong>programming</strong>, that's why I started studying Web Development 2 years ago. Since then, I've come a long way where I improved my skills and knowledge of programming languages and tools.</p>
-            <p></p>
-        </div>
-    )
-}
+  return (
+    <div className="about" id="about">
+      <h2>About Me</h2>
+      <p>
+        Hello! I'm 25 years old, working as a Tech Sourcer at{" "}
+        <strong>RocketPower</strong> and soon to study Web Development at{" "}
+        <strong>UADE</strong>. My goal is to work on a company as a{" "}
+        <strong>Frontend Developer</strong>.
+      </p>
+      <p>
+        I'm passionate about <strong>programming</strong>, that's why I started
+        studying Web Development 2 years ago. Since then, I've come a long way
+        where I improved my skills and knowledge of programming languages and
+        tools.
+      </p>
+    </div>
+  );
+};
 
-export default About
+export default About;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import './Home.scss';
-import croppedCvPicture from '../../assets/images/croppedCvPicture.png';
-import { RoughNotation } from "react-rough-notation";
+import React from "react";
+import "./Home.scss";
+import croppedCvPicture from "../../assets/images/croppedCvPicture.png";
 
 const Home = () => {
-    return (
-        <div className="home" id="home">
-                <div className="home_description">
-                    <h1>Hi there, I'm Santiago!</h1>
-                    <p>Welcome to my portfolio! Here you'll find my story, projects and a way to get in touch with me.</p>
-                    <p>I <RoughNotation type="highlight" animate="true" color="lightblue" show="true">love making apps</RoughNotation> that are user-friendly, simple and delightful. I am looking for my first <strong>Full Stack Development</strong> job.</p>
-                </div>
-            <div className="home_image"><img src={croppedCvPicture} alt="Profile" /></div>
-        </div>
-    )
-}
+  return (
+    <div className="home" id="home">
+      <div className="home_description">
+        <h1>Hi there, I'm Santiago!</h1>
+        <p>
+          Welcome to my portfolio! Here you'll find my story, projects and a way
+          to get in touch with me.
+        </p>
+        <p>
+          I love making apps that are user-friendly, simple and delightful. I am
+          looking for my first <strong>Frontend Development</strong> job.
+        </p>
+      </div>
+      <div className="home_image">
+        <img src={croppedCvPicture} alt="Profile" />
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
